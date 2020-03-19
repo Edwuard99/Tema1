961b5360ab09cc7ea93e95dda7e645116e1e3b70
Vlad Eduard Diaconescu <edidiaconescu@gmail.com>
2020-03-19 05:51:21 +0200

    First commit

---
+++ cmake-build-debug/CMakeFiles/Tema1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tema1.dir/main.cpp.obj"
+  "Tema1.exe"
+  "Tema1.exe.manifest"
+  "Tema1.pdb"
+  "libTema1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tema1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+using namespace std;
+
+class Nod{
+    int value;
+    Nod *next;
+    //Nod *prev;
+
+public:
+    Nod(int value){
+        this->value = value;
+    }
+
+    void setValue(int value){
+        this->value = value;
+    }
+    int getValue(){
+        return this->value;
+    }
+    void setNext(Nod *next){
+        this->next = next;
+    }
+    Nod *getNext(){
+        return this->next;
+    }
+    /*void setPrev(Nod *prev){
+        this->prev = prev;
+    }
+    Nod getPrev(){
+        return *prev;
+    }*/
+};
+
+class MultimeDinamic{
+    Nod first = NULL;
+    Nod current = NULL;
+
+public:
+
+    MultimeDinamic(int v[], int size){
+        first = Nod(v[0]);
+        current = first;
+        for(int i = 1; i < size; i++){
+            Nod nou = Nod(v[i]);
+            current.setNext(&nou);
+            current = nou;
+        }
+    }
+
+    void printMultime() {
+        current = first;
+        while(true){
+            cout << current.getValue() << ' ';
+            if(current.getNext() == NULL)
+                break;
+            current = *current.getNext();
+        }
+    }
+
+
+};
+
+/*void printVector(int v[], int size){
+    cout << sizeof(&v) << endl;
+    for(int i = 0; i <= sizeof(v)/4; i++){
+        cout << v[i] << ' ';
+    }
+}*/
+
+
+
+int main() {
+    /*Nod n1 = Nod(1);
+    Nod n2 = Nod(2);
+    Nod n3 = Nod(3);
+    Nod n4 = Nod(4);
+    Nod n5 = Nod(5);
+    Nod n6 = Nod(6);
+
+    n1.setNext(&n2);
+    n2.setNext(&n3);
+    n3.setNext(&n4);
+    n4.setNext(&n5);
+    n5.setNext(&n6);
+
+
+    Nod current = n1;
+    while(true){
+        cout << current.getValue() << ' ';
+        if(current.getNext() == NULL)
+            break;
+        current = *current.getNext();
+
+    }*/
+
+    int v[6] = {1, 2, 3, 4, 5, 6};
+    int size = sizeof(v)/4;
+
+    MultimeDinamic m = MultimeDinamic(v, size);
+    m.printMultime();
+
+
+    //printVector(v);
+
+
+
+    return 0;
+}
